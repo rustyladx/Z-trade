3a87b77542b840179e95e71a0c2c870bbe634a66
rustyladx <akashjeet2707@gmail.com>
2025-05-01 15:40:55 +0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-setInterval(async () => {
-  const res = await fetch("/api/signal");
-  const data = await res.json();
-  document.getElementById("alerts").innerText = 
-    `Signal: ${data.signal} ${data.symbol} at ${data.entry}`;
-}, 5000);
